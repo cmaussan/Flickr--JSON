429402ca2c3bcfcccb723f2f6f1b2ddd4240720a
cmaussan <camille.maussang@rtgi.fr>
2009-10-10 14:14:18 +0200

    initial commit

---
+++ lib/Flickr/JSON.pm
@@ -0,0 +1,33 @@
+package Flickr::JSON;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Flickr::JSON -
+
+=head1 SYNOPSIS
+
+  use Flickr::JSON;
+
+=head1 DESCRIPTION
+
+Flickr::JSON is
+
+=head1 AUTHOR
+
+Camille Maussang E<lt>camille.maussang@rtgi.frE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
